aa7787c2654509dc3a0c4aecaf4f7a648c160bc0
shaokt <shaorobics@gmail.com>
2016-03-19 11:10:31 -0400

    common function for get/post/patch
    - moved comment out into global functions
    - support for get/post/patch for different data

--- index.js
+++ index.js
@@ -14,24 +14,74 @@ app.use(function(req, res, next) {
 app.use(bodyParser.json({ type: 'application/*+json' }))
 var jsonParser = bodyParser.json()
 
-app.get('/resources', function (req, res) {
-	fs.readFile('./data/resources.json', function (err, data) {
-		if (err) {
-		   return console.error(err);
-		}
-		res.json(JSON.parse(data.toString()));
-		//console.log("Asynchronous read: " + data.toString());
-	});
-});
+// support for employees
+app.get('/resources', function(req, res) { return app.getData('./data/resources.json', res); });
+app.patch('/resources/:id', jsonParser, function(req, res) { return app.patchData(req, res, './data/resources.json'); });
+app.post('/resources', jsonParser, function(req, res){ return app.postData(req, res, './data/resources.json'); });
 
-app.get('/assignments', function (req, res) {
-	fs.readFile('./data/assignments.json', function (err, data) {
+// support for assignments
+app.get('/assignments', function(req, res) { return app.getData('./data/assignments.json', res); });
+app.patch('/assignments/:id', jsonParser, function(req, res) { return app.patchData(req, res, './data/assignments.json'); });
+app.post('/assignments', jsonParser, function(req, res){ return app.postData(req, res, './data/assignments.json'); });
+
+// load specified JSON file to get data
+app.getData = function(filePath, res) {
+	fs.readFile(filePath, function (err, data) {
 		if (err) {
 		   return console.error(err);
 		}
-		res.json(JSON.parse(data.toString()));
+		return res.json(JSON.parse(data.toString()));
 	});
-});
+};
+
+// patch data in specified file
+app.patchData = function(req, res, filePath) {
+	var id = req.params.id;
+    if (!req.body) return res.sendStatus(400)
+
+    fs.readFile(filePath, 'utf8', function (err, data) {
+        if (err){
+            throw err;
+            res.sendStatus(400)
+        }
+
+        var patches = JSON.parse(data.toString());
+
+        for(patch in patches.data) {
+            if(id == patches.data[patch].id){
+                patches.data[patch] = req.body.data
+                break;
+            }
+        }
+
+        fs.writeFile (filePath, JSON.stringify(patches, null, 2), function(err) {
+            if (err){
+                console.log('patch error: ' + error)
+                throw err;
+                res.sendStatus(400)
+            }
+        });
+    });
+	res.json(req.body);
+};
+
+// new data added, append to end of JSON
+app.postData = function(req, res, filePath) {
+	var id = req.params.id;
+    if (!req.body) return res.sendStatus(400)
+
+    fs.readFile(filePath, 'utf8', function (err, data) {
+        if (err) throw err;
+
+        var post = JSON.parse(data.toString());
+        post.data.push(req.body.data)
+
+        fs.writeFile (filePath, JSON.stringify(post, null, 2), function(err) {
+            if (err) throw err;
+        });
+    });
+	res.json(req.body);
+};
 
 app.get('/resources/:id', function(req, res) {
 	var id = req.params.id;
@@ -50,73 +100,4 @@ app.get('/resources/:id', function(req, res) {
 	});
 });
 
-// new person added, append to end of JSON
-app.post('/resources', jsonParser, function (req, res){
-	var id = req.params.id;
-    if (!req.body) return res.sendStatus(400)
-
-    fs.readFile('./data/resources.json', 'utf8', function (err, data) {
-        if (err) throw err;
-
-        var employees = JSON.parse(data.toString());
-        employees.data.push(req.body.data)
-
-        fs.writeFile ('./data/resources.json', JSON.stringify(employees, null, 2), function(err) {
-            if (err) throw err;
-            console.log('POST - added : ' + req.body.data.id);
-        });
-    });
-	res.json(req.body);
-    //res.sendStatus(200)
-});
-
-// person modified, patch file
-app.patch('/resources/:id', jsonParser, function (req, res) {
-	var id = req.params.id;
-    if (!req.body) return res.sendStatus(400)
-    //console.log(req.body)
-
-    //console.log('readFile : ' + req.body.data.attributes.name)
-    fs.readFile('./data/resources.json', 'utf8', function (err, data) {
-        if (err){
-            throw err;
-            res.sendStatus(400)
-        }
-
-        var employees = JSON.parse(data.toString());
-        //console.log(employees)
-        for(employee in employees.data) {
-            if(id == employees.data[employee].id){
-                employees.data[employee] = req.body.data
-                break;
-            }
-        }
-
-        //console.log(JSON.stringify(employees, null, 2))
-        console.log('replaced JSON: ' + req.body.data.attributes.name)
-
-        fs.writeFile ('./data/resources.json', JSON.stringify(employees, null, 2), function(err) {
-            if (err){
-                console.log('patch error: ' + error)
-                throw err;
-                res.sendStatus(400)
-            }
-            console.log('PATCH complete : ' + req.body.data.id + ' ' + req.body.data.attributes.name);
-        });
-    });
-	res.json(req.body);
-});
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-})
-/**/
-
-
-/*
-app.get('/', function(req, res){
-  res.send('hello world');
-});
-
-app.listen(3000);
-*/
+app.listen(3000, function () { console.log('Example app listening on port 3000!'); })
